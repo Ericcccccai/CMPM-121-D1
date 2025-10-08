f9e5d93ef2e907842ae125b330e3d3097502876e
Eric Cai <102574544+Ericcccccai@users.noreply.github.com>
2025-10-08 03:28:06 +0000

    added a button

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,8 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const button = document.createElement("button");
+button.textContent = "Click me!";
+button.onclick = () => alert("Hello from CMPM 121!");
+document.body.appendChild(button);
