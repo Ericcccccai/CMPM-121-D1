e5208c86b55ada83468e60be92a8e52663f0096b
Eric Cai <102574544+Ericcccccai@users.noreply.github.com>
2025-10-10 19:52:30 +0000

    added a counter with auto increase

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,25 @@ document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
 
+//button
 const button = document.createElement("button");
 button.textContent = "Click me!";
 button.onclick = () => alert("Hello from CMPM 121!");
 document.body.appendChild(button);
+
+//count
+let count = 0;
+const counterDisplay = document.createElement("div");
+counterDisplay.textContent = `Count: ${count}`;
+document.body.appendChild(counterDisplay);
+
+// Update on click
+button.onclick = () => {
+  count++;
+  counterDisplay.textContent = `Count: ${count}`;
+};
+
+setInterval(() => {
+  count++;
+  counterDisplay.textContent = `Count: ${count}`;
+}, 1000);
